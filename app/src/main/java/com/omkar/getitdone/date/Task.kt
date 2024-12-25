ea5ff85607b3033ccffb2640db3bca9a17e3bb96
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-12-25 16:19:51 +0530

    Change DB Schema

--- app/src/main/java/com/omkar/getitdone/date/Task.kt
+++ app/src/main/java/com/omkar/getitdone/date/Task.kt
@@ -1,12 +1,14 @@
 package com.omkar.getitdone.date
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "task")
 data class Task(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "task_id") @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
     val description: String? = null,
-    val isStarred: Boolean = false
+    @ColumnInfo(name = "is_starred") val isStarred: Boolean = false,
+    @ColumnInfo(name = "is_complete") val isComplete: Boolean = false
 )
